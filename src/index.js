03e103ac729ec8d06c959a192f666d82942f63e8
asucarlos <kuwahara.mee@gmail.com>
2020-04-12 19:09:07 -0400

    remove unused files under src and create new index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { PrimaryButton, SecondaryButton, TernaryButton } from "./components/buttons";
+import { GlobalStyle } from "./utils";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => (
+  <React.Fragment>
+    <PrimaryButton disabled>Hello World</PrimaryButton>
+    <SecondaryButton disabled>Hey Yo</SecondaryButton>
+    <TernaryButton disabled>Mhhhh</TernaryButton>
+    <GlobalStyle />
+  </React.Fragment>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
