e55405b14387605518d151ccfd04e724ef93104c
asucarlos <kuwahara.mee@gmail.com>
2020-05-10 23:40:04 -0400

    update readme

---
+++ storybook-static/precache-manifest.045e82ac3d1c6437bb8ff6d9d5bf440e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c19f474f780369390ba9a928c0f5acd0",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a316260a1c2c94dbc16b.bundle.js"
+  },
+  {
+    "url": "runtime~main.a316260a1c2c94dbc16b.bundle.js"
+  },
+  {
+    "revision": "e6cd71844b5344550bf6367224bb6b8b",
+    "url": "static/media/sign-up.e6cd7184.svg"
+  },
+  {
+    "url": "vendors~main.a316260a1c2c94dbc16b.bundle.js"
+  },
+  {
+    "url": "vendors~main.a316260a1c2c94dbc16b.bundle.js.LICENSE.txt"
+  }
+]);
